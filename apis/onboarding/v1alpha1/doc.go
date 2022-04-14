232a3f60d5a943cc42cb1e33048926225a889449
EC2 Default User <ec2-user@ip-10-0-1-207.ec2.internal>
2022-04-14 09:14:38 +0000

    clean(all)

--- apis/onboarding/v1alpha1/doc.go
+++
@@ -1,4 +0,0 @@
-// Package v1alpha1 contains API Schema definitions for the onboarding v1alpha1 API group
-// +k8s:deepcopy-gen=package,register
-// +groupName=onboarding.beopenit.com
-package v1alpha1
